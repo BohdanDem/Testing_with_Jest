e0e731004bc45adfc9313b1f64b9131302445b23
BohdanDem <bogdandemchuk.1@gmail.com>
2025-11-23 17:54:50 +0200

    Jest hooks and testing for errors

--- src/test/Utils.test.ts
+++ src/test/Utils.test.ts
@@ -1,6 +1,46 @@
-import {getStringInfo, toUpperCase} from "../app/Utils";
+import {getStringInfo, StringUtils, toUpperCase} from "../app/Utils";
 
 describe('Utils test suite', ()=>{
+  
+  describe('StringUtils tests', ()=>{
+    
+    let sut: StringUtils;
+    
+    beforeEach(()=>{
+      sut = new StringUtils();
+    })
+    
+    it('Should return correct upperCase', ()=>{
+      const actual = sut.toUpperCase('abc');
+      expect(actual).toBe('ABC');
+    })
+    
+    it('Should throw error on invalid argument - function', ()=>{
+      function expectError() {
+        const actual = sut.toUpperCase('');
+      }
+      expect(expectError).toThrow();
+      expect(expectError).toThrow('Invalid argument!');
+    })
+    
+    it('Should throw error on invalid argument - arrow function', ()=>{
+      expect(()=>{
+        sut.toUpperCase('');
+      }).toThrow('Invalid argument!');
+    })
+    
+    it('Should throw error on invalid argument - try catch block', ()=>{
+      try {
+        sut.toUpperCase('');
+        throw new Error('sut.toUpperCase did not throw error!');
+      } catch (error) {
+        expect(error).toBeInstanceOf(Error);
+        expect(error).toHaveProperty('message', 'Invalid argument!');
+      }
+      
+    })
+    
+  });
   
   it('should return uppercase of valid string', () => {
     //arrange:
@@ -15,7 +55,7 @@ describe('Utils test suite', ()=>{
   })
   
   //parametrized test
-  describe.only('ToUpperCase examples', ()=>{
+  describe('ToUpperCase examples', ()=>{
     it.each([
       {input:'abc', expected: 'ABC'},
       {input:'My-String', expected: 'MY-STRING'},
